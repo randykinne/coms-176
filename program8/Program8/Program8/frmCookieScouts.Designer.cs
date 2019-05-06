b46ca11808dde2f602fece5251ac6b88e1a09e84
Randy Kinne <r27kinne@gmail.com>
2019-05-06 14:40:18 -0400

    Add program 8 files via upload

---
+++ program8/Program8/Program8/frmCookieScouts.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Program8
+{
+    partial class frmCookieScouts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbxInfo = new System.Windows.Forms.GroupBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.lblInventory = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudSold = new System.Windows.Forms.NumericUpDown();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lbxCookies = new System.Windows.Forms.ListBox();
+            this.gbxInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSold)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bradley Hand ITC", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(21, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 79);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cookie Scouts";
+            // 
+            // gbxInfo
+            // 
+            this.gbxInfo.Controls.Add(this.lblName);
+            this.gbxInfo.Controls.Add(this.lblValue);
+            this.gbxInfo.Controls.Add(this.lblInventory);
+            this.gbxInfo.Controls.Add(this.lblPrice);
+            this.gbxInfo.Controls.Add(this.label5);
+            this.gbxInfo.Controls.Add(this.label4);
+            this.gbxInfo.Controls.Add(this.label3);
+            this.gbxInfo.Controls.Add(this.label2);
+            this.gbxInfo.Controls.Add(this.nudSold);
+            this.gbxInfo.Controls.Add(this.btnUpdate);
+            this.gbxInfo.ForeColor = System.Drawing.Color.White;
+            this.gbxInfo.Location = new System.Drawing.Point(420, 146);
+            this.gbxInfo.Name = "gbxInfo";
+            this.gbxInfo.Size = new System.Drawing.Size(252, 252);
+            this.gbxInfo.TabIndex = 2;
+            this.gbxInfo.TabStop = false;
+            this.gbxInfo.Text = "Cookie Info";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(18, 25);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(150, 25);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "Cookie Name";
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValue.Location = new System.Drawing.Point(188, 120);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblValue.Size = new System.Drawing.Size(39, 13);
+            this.lblValue.TabIndex = 8;
+            this.lblValue.Text = "80.92";
+            this.lblValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblInventory
+            // 
+            this.lblInventory.AutoSize = true;
+            this.lblInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInventory.Location = new System.Drawing.Point(206, 87);
+            this.lblInventory.Name = "lblInventory";
+            this.lblInventory.Size = new System.Drawing.Size(21, 13);
+            this.lblInventory.TabIndex = 7;
+            this.lblInventory.Text = "17";
+            this.lblInventory.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(195, 59);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(32, 13);
+            this.lblPrice.TabIndex = 6;
+            this.lblPrice.Text = "4.76";
+            this.lblPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Sold: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Inventory Value: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Inventory:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Price: ";
+            // 
+            // nudSold
+            // 
+            this.nudSold.Location = new System.Drawing.Point(107, 151);
+            this.nudSold.Name = "nudSold";
+            this.nudSold.Size = new System.Drawing.Size(120, 20);
+            this.nudSold.TabIndex = 2;
+            this.nudSold.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btnUpdate.Location = new System.Drawing.Point(152, 190);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbxCookies
+            // 
+            this.lbxCookies.FormattingEnabled = true;
+            this.lbxCookies.Location = new System.Drawing.Point(34, 146);
+            this.lbxCookies.Name = "lbxCookies";
+            this.lbxCookies.Size = new System.Drawing.Size(342, 251);
+            this.lbxCookies.TabIndex = 1;
+            this.lbxCookies.SelectedIndexChanged += new System.EventHandler(this.lbxCookies_SelectedIndexChanged);
+            // 
+            // frmCookieScouts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(709, 432);
+            this.Controls.Add(this.lbxCookies);
+            this.Controls.Add(this.gbxInfo);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCookieScouts";
+            this.Text = "Cookie Sales";
+            this.gbxInfo.ResumeLayout(false);
+            this.gbxInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSold)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbxInfo;
+        private System.Windows.Forms.Label lblInventory;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudSold;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ListBox lbxCookies;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ program8/Program8/Program8/frmCookieScouts.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program8
+{
+    public partial class frmCookieScouts : Form
+    {
+        public frmCookieScouts()
+        {
+            InitializeComponent();
+            
+            // Set up default values on program load
+            addCookie(new CookieData("Peppermint Flatties", decimal.Parse(4.99.ToString()), 8));
+            addCookie(new CookieData("Chocolate Chippers", decimal.Parse(4.76.ToString()), 17));
+            addCookie(new CookieData("Pecan Paradise", decimal.Parse(6.82.ToString()), 9));
+            addCookie(new CookieData("Sugary Shortcake", decimal.Parse(5.99.ToString()), 12));
+
+            // Select 1st item in cookie list
+            lbxCookies.SelectedIndex = 0;
+        }
+
+        // Init + assign new list for cookies to hold all of the cookie data
+        // Like a cookie jar
+        List<CookieData> cookies = new List<CookieData>();
+
+        // Method to find cookie data by name
+        private CookieData getCookie(String name)
+        {
+            // Go through all the cookies
+            foreach (CookieData cookie in cookies)
+            {
+                // Find the cookie in the cookie jar and pull it out
+                if (cookie.CookieName == name)
+                {
+                    return cookie;
+                }
+            }
+
+            // Cookie not found
+            // Luckily this shouldn't happen as this method is only called on SelectedIndexChange and Update, and in Update there will always be a selected cookie name
+            return null;
+        }
+        
+        // Add a cookie to the real jar that holds the actual cookie data and the fake jar that the user sees
+        private void addCookie(CookieData data)
+        {
+            cookies.Add(data);
+            lbxCookies.Items.Add(data.CookieName);
+        }
+
+        // Display cookie data to user
+        private void displayCookieData(CookieData data)
+        {
+            lblName.Text = data.CookieName;
+            lblPrice.Text = data.CookiePrice.ToString();
+            lblInventory.Text = data.CookieCount.ToString();
+            lblValue.Text = data.CookieValue.ToString();
+        }
+
+        // Event for listbox selected index change
+        private void lbxCookies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Display the cookie retrieved by name from the cookie jar
+            displayCookieData(getCookie(lbxCookies.Text));
+        }
+
+        // Update button click
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the cookie by name
+            CookieData cookie = getCookie(lbxCookies.Text);
+
+            // Ensure that there's enough cookies in stock to complete the sale
+            if (cookie.CookieCount - nudSold.Value < 0)
+            {
+                MessageBox.Show("Not enough items in stock to confirm this sale!");
+                return;
+            }
+
+            // Show the sale amount to the user
+            MessageBox.Show("Total Sale: $" + (cookie.CookiePrice * nudSold.Value).ToString("N2"));
+
+            // Subract the sold amount from the amount in stock
+            cookie.CookieCount -= int.Parse(nudSold.Value.ToString());
+
+            // Display the updated cookie data (works just like 'invoke the selectedindexchanged')
+            displayCookieData(cookie);
+
+            // Reset the sold value to 0 to get ready for the next sale
+            nudSold.Value = 0;
+        }
+    }
+
+    // COOKIE
+    class CookieData
+    {
+        // The public name of the cookie, isn't changed
+        public String CookieName { get; set; }
+
+        // Private var containing the hidden cookie price
+        private Decimal cookiePrice;
+
+        // Public property using custom setter that calls updateValue()
+        public Decimal CookiePrice
+        {
+            get { return cookiePrice; }
+            set
+            {
+                cookiePrice = value;
+                updateValue();
+            }
+        }
+
+        // Private var containing the hidden cookie count
+        private int cookieCount;
+
+        // Public property using custom setter that calls updateValue()
+        public int CookieCount {
+            get { return cookieCount;  }
+            set
+            {
+                cookieCount = value;
+                updateValue();
+            }
+        }
+
+        // Public property using private set to ensure that the value is only changed from within this class 
+        public Decimal CookieValue { get; private set; }
+
+        // Private method to set cookievalue, can only be called within the class and is only called in CookieCount and CookiePrice setters
+        private void updateValue()
+        {
+            CookieValue = CookiePrice * CookieCount;
+        }
+
+        // Initializer requires values because it would be bad if any of them were null
+        public CookieData(String name, Decimal price, int inventoryCount)
+        {
+            CookieName = name;
+            CookiePrice = price;
+            CookieCount = inventoryCount;
+        }
+    }
+}
