b1c119a692623a4c7405c743271195ed449ab090
Randy Kinne <r27kinne@gmail.com>
2019-05-06 14:44:44 -0400

    Add program 5 files via upload

---
+++ program5/program5/program5/frmColesApp.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace program5
+{
+    partial class frmColesApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmColesApp));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblSavings = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblSubTotal = new System.Windows.Forms.Label();
+            this.nudSubtotal = new System.Windows.Forms.NumericUpDown();
+            this.nudSalePercent = new System.Windows.Forms.NumericUpDown();
+            this.nudExtraDiscount = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSubtotal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSalePercent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudExtraDiscount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(46, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Indigo;
+            this.label1.Location = new System.Drawing.Point(43, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Subtotal:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.Color.Indigo;
+            this.label2.Location = new System.Drawing.Point(43, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sale Percent:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.ForeColor = System.Drawing.Color.Indigo;
+            this.label3.Location = new System.Drawing.Point(43, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Extra Discount Percent:";
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.lblSubTotal);
+            this.groupBox.Controls.Add(this.lblTax);
+            this.groupBox.Controls.Add(this.lblTotal);
+            this.groupBox.Controls.Add(this.lblSavings);
+            this.groupBox.Controls.Add(this.label7);
+            this.groupBox.Controls.Add(this.label6);
+            this.groupBox.Controls.Add(this.label5);
+            this.groupBox.Controls.Add(this.label4);
+            this.groupBox.Location = new System.Drawing.Point(316, 169);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(218, 113);
+            this.groupBox.TabIndex = 4;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Receipt";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.ForeColor = System.Drawing.Color.Indigo;
+            this.btnCalculate.Location = new System.Drawing.Point(46, 242);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 40);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.Indigo;
+            this.btnClear.Location = new System.Drawing.Point(127, 242);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 40);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Indigo;
+            this.btnExit.Location = new System.Drawing.Point(208, 242);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 40);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.ForeColor = System.Drawing.Color.Indigo;
+            this.label4.Location = new System.Drawing.Point(6, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Subtotal:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.ForeColor = System.Drawing.Color.Indigo;
+            this.label5.Location = new System.Drawing.Point(6, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Tax";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.ForeColor = System.Drawing.Color.Indigo;
+            this.label6.Location = new System.Drawing.Point(6, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Grand Total:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Yellow;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(6, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "You Saved:";
+            // 
+            // lblSavings
+            // 
+            this.lblSavings.AutoSize = true;
+            this.lblSavings.BackColor = System.Drawing.Color.Yellow;
+            this.lblSavings.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSavings.ForeColor = System.Drawing.Color.Red;
+            this.lblSavings.Location = new System.Drawing.Point(162, 75);
+            this.lblSavings.Name = "lblSavings";
+            this.lblSavings.Size = new System.Drawing.Size(32, 13);
+            this.lblSavings.TabIndex = 15;
+            this.lblSavings.Text = "0.00";
+            this.lblSavings.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.BackColor = System.Drawing.Color.White;
+            this.lblTotal.ForeColor = System.Drawing.Color.Indigo;
+            this.lblTotal.Location = new System.Drawing.Point(166, 44);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(28, 13);
+            this.lblTotal.TabIndex = 16;
+            this.lblTotal.Text = "0.00";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.BackColor = System.Drawing.Color.White;
+            this.lblTax.ForeColor = System.Drawing.Color.Indigo;
+            this.lblTax.Location = new System.Drawing.Point(166, 31);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(28, 13);
+            this.lblTax.TabIndex = 17;
+            this.lblTax.Text = "0.00";
+            this.lblTax.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblSubTotal
+            // 
+            this.lblSubTotal.AutoSize = true;
+            this.lblSubTotal.BackColor = System.Drawing.Color.White;
+            this.lblSubTotal.ForeColor = System.Drawing.Color.Indigo;
+            this.lblSubTotal.Location = new System.Drawing.Point(166, 16);
+            this.lblSubTotal.Name = "lblSubTotal";
+            this.lblSubTotal.Size = new System.Drawing.Size(28, 13);
+            this.lblSubTotal.TabIndex = 18;
+            this.lblSubTotal.Text = "0.00";
+            this.lblSubTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // nudSubtotal
+            // 
+            this.nudSubtotal.DecimalPlaces = 2;
+            this.nudSubtotal.Location = new System.Drawing.Point(176, 153);
+            this.nudSubtotal.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.nudSubtotal.Name = "nudSubtotal";
+            this.nudSubtotal.Size = new System.Drawing.Size(120, 20);
+            this.nudSubtotal.TabIndex = 8;
+            this.nudSubtotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // nudSalePercent
+            // 
+            this.nudSalePercent.Location = new System.Drawing.Point(176, 182);
+            this.nudSalePercent.Name = "nudSalePercent";
+            this.nudSalePercent.Size = new System.Drawing.Size(120, 20);
+            this.nudSalePercent.TabIndex = 9;
+            this.nudSalePercent.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // nudExtraDiscount
+            // 
+            this.nudExtraDiscount.Location = new System.Drawing.Point(176, 209);
+            this.nudExtraDiscount.Name = "nudExtraDiscount";
+            this.nudExtraDiscount.Size = new System.Drawing.Size(120, 20);
+            this.nudExtraDiscount.TabIndex = 10;
+            this.nudExtraDiscount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // frmColesApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(561, 311);
+            this.Controls.Add(this.nudExtraDiscount);
+            this.Controls.Add(this.nudSalePercent);
+            this.Controls.Add(this.nudSubtotal);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmColesApp";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmColesApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSubtotal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSalePercent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudExtraDiscount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblSubTotal;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblSavings;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudSubtotal;
+        private System.Windows.Forms.NumericUpDown nudSalePercent;
+        private System.Windows.Forms.NumericUpDown nudExtraDiscount;
+    }
+}
+
---
+++ program5/program5/program5/frmColesApp.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program5
+{
+    public partial class frmColesApp : Form
+    {
+        public frmColesApp()
+        {
+            InitializeComponent();
+        }
+
+        // Reset the reciept text to the default values
+        private void reset()
+        {
+            lblSubTotal.Text = "0.00";
+            lblTax.Text = "0.00";
+            lblTotal.Text = "0.00";
+            lblSavings.Text = "0.00";
+        }
+
+        // Get savings from a subtotal
+        private double getSavings(double subTotal, double total)
+        {
+            return subTotal - total;
+        }
+
+        // Get discount taken off subtotal
+        private double getDiscount(double percent, double subTotal)
+        {
+            return subTotal * (1 - percent);
+        }
+
+        // Get tax, default 6%
+        private double getTax(double subTotal)
+        {
+            return subTotal * 0.06;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmColesApp_Load(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Set the subtotal label to the selected subtotal
+            lblSubTotal.Text = nudSubtotal.Text;
+
+            // Parse these values and init+assign variables
+            double total = double.Parse(nudSubtotal.Text);
+            double salePercent = double.Parse(nudSalePercent.Text)/100;
+            double extraPercent = double.Parse(nudExtraDiscount.Text)/100;
+
+            // Run getDiscount() twice to hit both sale percentage and extra discount percentage
+            total = getDiscount(salePercent, total);
+            total = getDiscount(extraPercent, total);
+
+            // Set tax label to getTax()
+            lblTax.Text = getTax(total).ToString("N2");
+
+            // Display before tax is added, otherwise value is incorrect
+            lblSavings.Text = getSavings(double.Parse(lblSubTotal.Text), total).ToString("N2");
+
+            // Actually add tax to total
+            total += getTax(total);
+
+            // Display final value to reciept
+            lblTotal.Text = total.ToString("N2");
+        }
+    }
+}
